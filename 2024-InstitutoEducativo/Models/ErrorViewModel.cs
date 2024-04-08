21aa1c79aab5d8ef15736637edbe06121c4cc324
Ignacio Rosello <ignacioroselloir@gmail.com>
2024-04-08 20:42:51 -0300

    Creacion del proyecto.

---
+++ 2024-InstitutoEducativo/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace _2024_InstitutoEducativo.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
