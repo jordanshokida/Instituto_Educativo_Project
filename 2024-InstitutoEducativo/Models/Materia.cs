e5e786ec783050fd3a20596d45a19c7ffb3d4ca8
JuanRetamozo <juanma250891@gmail.com>
2024-04-08 22:30:02 -0300

    Creacion de modelos de Materia y MateriaCursadas

---
+++ 2024-InstitutoEducativo/Models/Materia.cs
@@ -0,0 +1,22 @@
+﻿namespace _2024_InstitutoEducativo.Models
+{
+    public class Materia
+    {
+        public int Id { get; set; }
+
+        public string MateriaNome { get; set; }
+
+        public int CodMateria { get; set; }
+
+        public string Descripcion { get; set; }
+
+        public int CupoMaximo { get; set; }
+
+        public string MateriaCursadas { get; set; }
+
+        public string Calificaciones { get; set; }
+
+        public string carrera { get; set; }
+
+    }
+}
---
+++ 2024-InstitutoEducativo/Models/MateriaCursada.cs
@@ -0,0 +1,23 @@
+﻿namespace _2024_InstitutoEducativo.Models
+{
+    public class MateriaCursada
+    {
+        public int Id { get; set; }
+
+        public string Nombre { get; set; }
+
+        public int Anio { get; set; }
+
+        public string Cuatrimestre { get; set; }
+
+        public string Activo { get; set; }
+
+        public string Materia { get; set; }
+
+        public string Profesor { get; set; }
+
+        public string Alumnos { get; set; }
+
+        public string Calificaciones { get; set; }  
+    }
+}
