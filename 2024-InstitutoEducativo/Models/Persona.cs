6635702e7425527fc09f1326fedf812586e49e6e
jordanshokida <jordii.s@hotmail.com>
2024-04-15 20:25:22 -0300

    Agregación de required en clase Persona

--- 2024-InstitutoEducativo/Models/Persona.cs
+++ 2024-InstitutoEducativo/Models/Persona.cs
@@ -1,25 +1,28 @@
-﻿namespace _2024_InstitutoEducativo.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace _2024_InstitutoEducativo.Models
 {
     public class Persona
 
     {  
         public int Id { get; set; }
+        [Required]  
         public string Nombre { get; set; }
-
+        [Required]
         public string Apellido { get; set;}
-
+        [Required]
         public string Email { get; set;}
-
+        
         public Telefono Telefono { get; set;}
 
         public List<Telefono> Telefonos { get; set;}
 
         public Direccion Direccion { get; set; }
-
+        [Required]
         public DateTime FechaAlta { get; set; } = DateTime.Now.Date;
-
+        [Required]
         public String Password { get; set;}
-
+        [Required]
         public string Dni { get; set; }
 
         public string NombreCompleto
