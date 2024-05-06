f53ffa596500c5b1efeecfe5c07ab7adc70a183d
Ignacio Rosello <ignacioroselloir@gmail.com>
2024-05-06 19:29:10 -0300

    Hago restricciones clase telefono

--- 2024-InstitutoEducativo/Models/Telefono.cs
+++ 2024-InstitutoEducativo/Models/Telefono.cs
@@ -1,11 +1,21 @@
-﻿namespace _2024_InstitutoEducativo.Models
+﻿using _2024_InstitutoEducativo.Helpers;
+using System.ComponentModel.DataAnnotations;
+
+namespace _2024_InstitutoEducativo.Models
 {
     public class Telefono
     {
+        [Key, ForeignKey("Telefono")]
         public int Id { get; set; }
 
-        public int CodArea { get; set; }
+        [Required(ErrorMessage = ErrorMsge.Required)]
+        [RegularExpression("@[0-9]*", ErrorMessage = ErrorMsge.OnlyNumbers)]
+        [StringLength(5, MinimumLength = 2, ErrorMessage = ErrorMsge.StringMaxMin)]
+        public String CodArea { get; set; }
 
+        [Required(ErrorMessage = ErrorMsge.Required)]
+        [RegularExpression("@[0-9]*", ErrorMessage = ErrorMsge.OnlyNumbers)]
+        [StringLength(10, MinimumLength = 6, ErrorMessage = ErrorMsge.StringMaxMin)]
         public string Numero { get; set;}
 
         public int PersonaId { get; set;}
