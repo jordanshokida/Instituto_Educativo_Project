cf4fd65b9e4c01c9dd7898d85093a98ce0be7b4f
Ignacio Rosello <ignacioroselloir@gmail.com>
2024-06-03 21:38:19 -0300

    Desarrollo modelo Rol

--- 2024-InstitutoEducativo/Models/Rol.cs
+++ 2024-InstitutoEducativo/Models/Rol.cs
@@ -7,24 +7,25 @@ namespace _2024_InstitutoEducativo.Models
     public class Rol : IdentityRole<int>
     {
 
-        //public int Id {  get; set; }
-        #region Constructores
-        public Rol() :base(){}
 
-        public Rol(string name) : base(name) { }
-        #endregion
 
-        #region Propiedades
-        [Display(Name = Alias.RoleName)]
-        public override string Name {
-            get {  return base.Name; }
-            set {  base.Name = value; }
-                }
+        public Rol() : base() { }
 
-        public override string NormalizedName { 
-            get => base.NormalizedName; 
-            set => base.NormalizedName = value; 
+        public Rol(string rolName) : base(rolName) { }
+
+        [Display(Name = "Nombre")]
+        public override string Name
+        {
+            get { return base.Name; }
+            set { base.Name = value; }
         }
-        #endregion
+
+        public override string NormalizedName
+        {
+            get => base.NormalizedName;
+            set => base.NormalizedName = value;
+        }
+
     }
 }
+
