e168f4fdcd71e401002e9f4c2a671dd1419a1d5c
Ignacio Rosello <ignacioroselloir@gmail.com>
2024-06-21 18:07:10 -0300

    Desarrollo de la precarga. Creacion de Helpers para nomenclar roles

---
+++ 2024-InstitutoEducativo/Helpers/Configs.cs
@@ -0,0 +1,12 @@
+﻿namespace _2024_InstitutoEducativo.Helpers
+{
+    public static class Configs
+    {
+        public const string PasswordGenerica = "Password1!";
+        public const string ProfesorRolName = "ProfesorRol";
+        public const string AdminRolName = "AdminRol";
+        public const string EmpleadoRolName = "EmpleadoRol";
+        public const string AlumnoRolName = "ClienteRol";
+        public const string AdminRolRame = "AdminRol";
+    }
+}
